51f8fb544cb0eebdf2f827db22d7bf819e950631
Ayushi-garg-cs <ayushigarg2346@gmail.com>
2026-04-04 21:24:00 +0530

    updated

---
+++ src/main/java/com/project/modal/Genre.java
@@ -0,0 +1,24 @@
+package com.project.modal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Genre {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank(message="Genre code is mandatory")
+    private String code;
+
+    @NotBlank(message="Genre name is mandatory")
+    private String name;
+
+    @Size(max=500,message="description must not exceed 500 characters")
+    private String description;
+}
